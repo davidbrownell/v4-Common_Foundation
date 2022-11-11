davidbrownell/v4-Common_Foundation

commit 7bec2c99d3f22403cd50d9fe6e6bb04802f62bf3

🎨[refactor] Changes for release CI
- 🐛 [-bug] Fixed compiler issue when calculating `input_root` with only a single file
- 🐛 [-bug] Fixed issue during setup where old foundation repo would be kept (even if the repo no longer existed)
- ✨ [+feature] `DevEnvScripts` can be invoked to provide the source python location
- ✨ [+feature] `CommitEmojis` can take input from a file (to work around command line issues where the first char is '-')

RepositoryBootstrap/Impl/Setup.py
@@ -138,6 +138,8 @@ def Setup(
                 ] = []
 
                 activities += [
+                    # Note that setup bootstrap MUST be the first activity invoked, as it creates
+                    # information leveraged by the other activities.
                     lambda **kwargs: _SetupBootstrap(
                         **{
                             **kwargs,
@@ -752,8 +754,23 @@ def _SetupBootstrap(
             ],
         )
 
+    # Find the foundation repository
+    foundation_repo = None
+    foundation_repo_id = uuid.UUID("DD6FCD30-B043-4058-B0D5-A6C8BC0374F4")
+
+    for repo in all_repos:
+        if repo.id == foundation_repo_id:
+            foundation_repo = repo
+            break
+
+    assert foundation_repo is not None
+
+    # Update the environment so this value is used by the other activities
+    os.environ[Constants.DE_FOUNDATION_ROOT_NAME] = str(foundation_repo.root)
+
+    # Create the bootstrap data
     EnvironmentBootstrap(
-        Utilities.GetFoundationRepository(),
+        foundation_repo.root,
         repo_data.configurations,
         fingerprints,
         dependencies,

.github/actions/activate/action.yml
@@ -9,7 +9,7 @@
 #
 #     Code Generator:         Jinja2
 #     Input Filename:         actions/activate/action.jinja2.yml
-#     Generated Date:         2022-11-08 16:40:54.005003
+#     Generated Date:         2022-11-09
 #
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------

.github/actions/builder/action.yml
@@ -9,7 +9,7 @@
 #
 #     Code Generator:         Jinja2
 #     Input Filename:         actions/builder/action.jinja2.yml
-#     Generated Date:         2022-11-08 16:40:54.007965
+#     Generated Date:         2022-11-09
 #
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------

.github/actions/checkout_source/action.yml
@@ -9,7 +9,7 @@
 #
 #     Code Generator:         Jinja2
 #     Input Filename:         actions/checkout_source/action.jinja2.yml
-#     Generated Date:         2022-11-08 16:40:54.009495
+#     Generated Date:         2022-11-09
 #
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------

.github/actions/dump_contexts/action.yml
@@ -9,7 +9,7 @@
 #
 #     Code Generator:         Jinja2
 #     Input Filename:         actions/dump_contexts/action.jinja2.yml
-#     Generated Date:         2022-11-08 16:40:54.014503
+#     Generated Date:         2022-11-09
 #
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------
@@ -37,25 +37,44 @@ runs:
   using: composite
   steps:
     - name: Dump GitHub context
-      run: echo '${{ toJSON(github) }}'
       shell: bash
+      run: |
+        echo "github:"
+        echo ""
+        echo '${{ toJSON(github) }}'
+
 
     - name: Dump job context
-      run: echo '${{ toJSON(job) }}'
       shell: bash
+      run: |
+        echo "job:"
+        echo ""
+        echo '${{ toJSON(job) }}'
 
     - name: Dump steps context
-      run: echo '${{ toJSON(steps) }}'
       shell: bash
+      run: |
+        echo "steps:"
+        echo ""
+        echo '${{ toJSON(steps) }}'
 
     - name: Dump runner context
-      run: echo '${{ toJSON(runner) }}'
       shell: bash
+      run: |
+        echo "runner:"
+        echo ""
+        echo '${{ toJSON(runner) }}'
 
     - name: Dump strategy context
-      run: echo '${{ toJSON(strategy) }}'
       shell: bash
+      run: |
+        echo "strategy:"
+        echo ""
+        echo '${{ toJSON(strategy) }}'
 
     - name: Dump matrix context
-      run: echo '${{ toJSON(matrix) }}'
       shell: bash
+      run: |
+        echo "matrix:"
+        echo ""
+        echo '${{ toJSON(matrix) }}'

.github/actions/initialize/action.yml
@@ -9,7 +9,7 @@
 #
 #     Code Generator:         Jinja2
 #     Input Filename:         actions/initialize/action.jinja2.yml
-#     Generated Date:         2022-11-08 16:40:54.016506
+#     Generated Date:         2022-11-09
 #
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------

.github/actions/setup/action.yml
@@ -9,7 +9,7 @@
 #
 #     Code Generator:         Jinja2
 #     Input Filename:         actions/setup/action.jinja2.yml
-#     Generated Date:         2022-11-08 16:40:54.021505
+#     Generated Date:         2022-11-09
 #
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------

.github/actions/tester/action.yml
@@ -9,7 +9,7 @@
 #
 #     Code Generator:         Jinja2
 #     Input Filename:         actions/tester/action.jinja2.yml
-#     Generated Date:         2022-11-08 16:40:54.023504
+#     Generated Date:         2022-11-09
 #
 # ----------------------------------------------------------------------
 # ----------------------------------------------------------------------

commit 1447311d74870733b5c3363b3da4046c16645108

🐛 [-bug] Fixed regression
Fixed regression associated with 7bec2c99d3f22403cd50d9fe6e6bb04802f62bf3.

Repos that have a direct dependency on Common_Foundation will have a valid link to that repo, those that don't have a direct dependency will not. The fix is to use that information if we have it, but revert to prior behavior if we don't (which should be ok in all scenarios, as indirect dependencies will not be impacted by the issue that was addressed by the previous change).

RepositoryBootstrap/Impl/Setup.py
@@ -763,7 +763,7 @@ def _SetupBootstrap(
             ],
         )
 
-    # Find the foundation repository
+    # Find the foundation repository (we will only find this if the repo has a direct dependency on it)
     foundation_repo = None
     foundation_repo_id = uuid.UUID("DD6FCD30-B043-4058-B0D5-A6C8BC0374F4")
 
@@ -772,14 +772,14 @@ def _SetupBootstrap(
             foundation_repo = repo
             break
 
-    assert foundation_repo is not None
+    foundation_repo_root = foundation_repo.root if foundation_repo else Utilities.GetFoundationRepositoryRoot()
 
     # Update the environment so this value is used by the other activities
-    os.environ[Constants.DE_FOUNDATION_ROOT_NAME] = str(foundation_repo.root)
+    os.environ[Constants.DE_FOUNDATION_ROOT_NAME] = str(foundation_repo_root)
 
     # Create the bootstrap data
     EnvironmentBootstrap(
-        foundation_repo.root,
+        foundation_repo_root,
         repo_data.configurations,
         fingerprints,
         dependencies,

RepositoryBootstrap/Impl/Utilities.py
@@ -32,7 +32,7 @@ from .. import DataTypes
 # |  Public Functions
 # |
 # ----------------------------------------------------------------------
-def GetFoundationRepository() -> Path:
+def GetFoundationRepositoryRoot() -> Path:
     """Returns the path to the foundation repository"""
 
     # Try to get the value from the environment