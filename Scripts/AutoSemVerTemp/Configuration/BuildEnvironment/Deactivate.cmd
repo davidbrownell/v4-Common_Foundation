4a508bd6648a9bcc6c51afa6972e1832ca26d5b5
David Brownell <db@DavidBrownell.com>
2023-03-04 12:26:59 -0800

    🎨 [refactor] Updated AutoSemVer interface (Change 3 of 3)

--- Scripts/AutoSemVerTemp/Configuration/BuildEnvironment/Deactivate.cmd
+++
@@ -1,6 +0,0 @@
-@echo off
-pushd "%~dp0"
-call "C:\Code\v4\Common\Foundation\RepositoryBootstrap\Impl\Deactivate.cmd" %*
-set _DEVELOPMENT_ENVIRONMENT_DEACTIVATE_ERROR=%ERRORLEVEL%
-popd
-if %_DEVELOPMENT_ENVIRONMENT_DEACTIVATE_ERROR% NEQ 0 (exit /B %_DEVELOPMENT_ENVIRONMENT_DEACTIVATE_ERROR%)
