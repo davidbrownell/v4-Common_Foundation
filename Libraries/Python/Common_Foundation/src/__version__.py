7b174ef1ec21ea1b67dd82c459fa3fdd8f007bd0
David Brownell <db@DavidBrownell.com>
2023-03-04 16:21:28 -0800

    🎨 [refactor] Added dynamic version gen based on AutoSemVer

---
+++ Libraries/Python/Common_Foundation/src/__version__.py
@@ -0,0 +1,21 @@
+# ----------------------------------------------------------------------
+# |
+# |  __version__.py
+# |
+# |  David Brownell <db@DavidBrownell.com>
+# |      2023-03-03 14:21:57
+# |
+# ----------------------------------------------------------------------
+# |
+# |  Copyright David Brownell 2023
+# |  Distributed under the Boost Software License, Version 1.0. See
+# |  accompanying file LICENSE_1_0.txt or copy at
+# |  http://www.boost.org/LICENSE_1_0.txt.
+# |
+# ----------------------------------------------------------------------
+
+# Note that this value is hard-coded, but verified via ../../ValidateLibraryVersions.py as a part of
+# the CI process. This custom validation is only necessary for these libraries in this foundation
+# repository. See the comments in ../../ValidateLibraryVersions.py for more information.
+
+VERSION = "0.3.0"
