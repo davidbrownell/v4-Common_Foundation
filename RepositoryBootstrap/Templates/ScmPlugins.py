10802c82d95524fa7773fdbb0123671122692d4b
David Brownell <db@davidbrownell.com>
2023-07-14 16:25:47 -0400

    🎨 [refactor] Repository generation implemented with cookiecutter
    Using cookie cutter rather than a custom solution. (commit 3 of 3)

--- RepositoryBootstrap/Templates/ScmPlugins.py
+++
@@ -1,8 +0,0 @@
-# pylint: disable=invalid-name, missing-module-docstring
-
-from RepositoryBootstrap.DataTypes import SCMPlugin  # type: ignore  # pylint: disable=import-error
-
-
-# ----------------------------------------------------------------------
-def GetPlugins() -> list[SCMPlugin]:
-    raise Exception("Implement me")
